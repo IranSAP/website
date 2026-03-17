4135a065c3b4b753d4cbecead414ae7f0c5be507
IranSAP <266448127+IranSAP@users.noreply.github.com>
2026-03-17 21:23:00 +0100

    improve multilanguage

--- src/i18n/de.js
+++ src/i18n/de.js
@@ -1,14 +1,18 @@
 export default {
+  lang: "de",
+  dir: "ltr",
 
-aboutTitle: "Was ist SAP?",
+  aboutTitle: "Was ist SAP?",
 
-block1Title: "SAP Unternehmen",
-block1Text: "SAP ist eines der weltweit führenden Unternehmen für Unternehmenssoftware. Die Software hilft Organisationen dabei Geschäftsprozesse wie Finanzen, Logistik, Personalwesen und Kundenmanagement zu steuern.",
+  block1Title: "SAP Unternehmen",
+  block1Text:
+    "SAP ist eines der weltweit führenden Unternehmen für Unternehmenssoftware.",
 
-block2Title: "ERP Systeme",
-block2Text: "SAP entwickelte einige der bekanntesten ERP Systeme der Welt. Diese Systeme integrieren Daten aus verschiedenen Abteilungen und ermöglichen effizientere Geschäftsprozesse.",
+  block2Title: "ERP Systeme",
+  block2Text:
+    "SAP entwickelte einige der bekanntesten ERP Systeme der Welt.",
 
-block3Title: "Globale Nutzung",
-block3Text: "SAP Technologien werden heute von tausenden Unternehmen weltweit in Branchen wie Industrie, Energie, Logistik, Finanzen und Handel eingesetzt."
-
+  block3Title: "Globale Nutzung",
+  block3Text:
+    "SAP Technologien werden heute weltweit in vielen Industrien eingesetzt."
 }
