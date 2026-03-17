b0349b183b925ac4514a115dbc5888ef748e97e0
IranSAP <266448127+IranSAP@users.noreply.github.com>
2026-03-17 22:15:24 +0100

    multilanguage bugfix

--- src/i18n/fa.js
+++ src/i18n/fa.js
@@ -1,18 +1,17 @@
 export default {
-  lang: "fa",
-  dir: "rtl",
 
-  aboutTitle: "SAP چیست؟",
+lang: "fa",
+dir: "rtl",
 
-  block1Title: "شرکت SAP",
-  block1Text:
-    "SAP یکی از بزرگترین شرکت های نرم افزار سازمانی در جهان است.",
+aboutTitle: "SAP چیست؟",
 
-  block2Title: "سیستم های ERP",
-  block2Text:
-    "SAP برخی از مهم ترین سیستم های ERP جهان را توسعه داده است.",
+block1Title: "شرکت SAP",
+block1Text: "SAP یکی از بزرگترین شرکت های نرم افزار سازمانی در جهان است.",
 
-  block3Title: "استفاده جهانی",
-  block3Text:
-    "فناوری های SAP امروزه در هزاران شرکت در سراسر جهان استفاده می شوند."
+block2Title: "سیستم های ERP",
+block2Text: "SAP برخی از مهم ترین سیستم های ERP جهان را توسعه داده است.",
+
+block3Title: "استفاده جهانی",
+block3Text: "فناوری های SAP امروزه در هزاران شرکت استفاده می شوند."
+
 }
